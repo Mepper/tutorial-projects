473cd4a95d14613864f9dfb7ec32cc75a69ef988
Fernando Molina <fernando.molina@globaltms.es>
2018-07-13 15:04:53 +0200

    first commit

---
+++ MyMVA(ASP.NET_2.0)/Pages/Account/Lockout.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyMVA_ASP.NET_2._0_.Pages.Account
+{
+    public class LockoutModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ MyMVA(ASP.NET_2.0)/Pages/Account/Manage/ShowRecoveryCodes.cshtml.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyMVA_ASP.NET_2._0_.Pages.Account.Manage
+{
+    public class ShowRecoveryCodesModel : PageModel
+    {
+        public string[] RecoveryCodes { get; private set; }
+
+        public IActionResult OnGet()
+        {
+            RecoveryCodes = (string[])TempData["RecoveryCodes"];
+            if (RecoveryCodes == null)
+            {
+                return RedirectToPage("TwoFactorAuthentication");
+            }
+
+            return Page();
+        }
+    }
+}
